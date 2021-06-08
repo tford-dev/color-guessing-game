da8025c6376e458f6db79d0ce4442fe7c36e1a80
tford-dev <terranceford513@gmail.com>
2021-06-08 18:23:25 -0400

    added favicon

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+const favicon = require("serve-favicon");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/static', express.static('public/'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
@@ -14,6 +16,6 @@ app.get('/', function(req, res) {
 //     console.log("Running on port 4000");
 // });
 
- app.listen(process.env.PORT, process.env.IP, function() {
-     console.log("Project Color Guessing Game is now online")
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Project Color Guessing Game is now online")
 });
